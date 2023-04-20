c925cccf914f19c111d059aca64f34a6c738c6c8
HungPD <hungpd170501@gmail.com>
2023-04-21 02:52:41 +0700

    initial commit

---
+++ api-gateway-service/src/test/java/com/example/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ eureka-discovery-service/src/test/java/com/example/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
